12bc55d29c1f67e13629d69992fad7a2e6e7dea1
Guilherme Vieira Moreira de Freitas <83255622+GuilhermeVgl@users.noreply.github.com>
2023-11-27 17:05:19 -0400

    Implementação classe Pessoa e Cliente

---
+++ src/main/java/com/guilherme/hotel/entities/Cliente.java
@@ -0,0 +1,10 @@
+package com.guilherme.hotel.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "cliente")
+public class Cliente extends Pessoa{
+
+}
--- src/main/java/com/guilherme/hotel/entities/Pessoa.java
+++ src/main/java/com/guilherme/hotel/entities/Pessoa.java
@@ -1,5 +1,143 @@
 package com.guilherme.hotel.entities;
 
-public class Pessoa {
-    
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+@MappedSuperclass
+@Inheritance(strategy = InheritanceType.JOINED)
+public abstract class Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+    private String endereco;
+    private String cep;
+    private String bairro;
+    private String cidade;
+    private String UF;
+    private String email;
+    private String telefone;
+
+    @Column(name = "data_nascimento")
+    private Date dataNascimento;
+    private String rg;
+    private String cpf;
+
+    public Pessoa() { }
+
+    public Pessoa(
+            String nome,
+            String endereco,
+            String cep,
+            String bairro,
+            String cidade,
+            String UF,
+            String email,
+            String telefone,
+            Date dataNascimento,
+            String rg,
+            String cpf)
+    {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.cep = cep;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.UF = UF;
+        this.email = email;
+        this.telefone = telefone;
+        this.dataNascimento = dataNascimento;
+        this.rg = rg;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getUF() {
+        return UF;
+    }
+
+    public void setUF(String UF) {
+        this.UF = UF;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 }
