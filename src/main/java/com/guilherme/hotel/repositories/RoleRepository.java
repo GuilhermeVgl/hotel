23d19eb30ba6f7d7faf47937239c775e318af4de
Guilherme Vieira Moreira de Freitas <83255622+GuilhermeVgl@users.noreply.github.com>
2023-11-29 10:07:03 -0400

    JwtAuthentication

---
+++ src/main/java/com/guilherme/hotel/repositories/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.guilherme.hotel.repositories;
+
+import com.guilherme.hotel.models.ERole;
+import com.guilherme.hotel.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(ERole name);
+}
