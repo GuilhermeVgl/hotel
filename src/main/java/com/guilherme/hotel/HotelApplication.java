9e7d0cdffcdfa5a3c03d0dc06a1a8a51571a3734
guilherme <guilhermeharfy@gmail.com>
2023-11-29 22:55:23 -0400

    autenticação com jwt geração de tokens

--- src/main/java/com/guilherme/hotel/HotelApplication.java
+++ src/main/java/com/guilherme/hotel/HotelApplication.java
@@ -2,7 +2,11 @@ package com.guilherme.hotel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+import com.guilherme.hotel.config.RsaKeyProperties;
+
+@EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication
 public class HotelApplication {
 
--- src/main/java/com/guilherme/hotel/controllers/HomeController.java
+++ src/main/java/com/guilherme/hotel/controllers/HomeController.java
@@ -1,5 +1,7 @@
 package com.guilherme.hotel.controllers;
 
+import java.security.Principal;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
-        return "Hello, Home!";
+    public String home(Principal principal) {
+        return "Hello, " + principal.getName();
     }
 
     @GetMapping("/secured")
