69e2a072d90182d4b58f2178ec4c58b2b590a70f
guilherme <guilhermeharfy@gmail.com>
2023-11-29 21:39:05 -0400

    :Feat: inclusão de login através do github

---
+++ src/main/java/com/guilherme/hotel/controllers/HomeController.java
@@ -0,0 +1,19 @@
+package com.guilherme.hotel.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello, Home!";
+    }
+
+    @GetMapping("/secured")
+    public String secured() {
+        return "Hello, secured!";
+    }
+
+}
